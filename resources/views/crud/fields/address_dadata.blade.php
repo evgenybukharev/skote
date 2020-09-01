2d86c3c22c5fa76ecc9290f5a73498b2625d6b1b
bukharev.evgeny <bukharev.evgeny@ya.ru>
2020-09-01 15:52:22 +0300

    dadata crud

--- resources/views/crud/fields/address_dadata.blade.php
+++ resources/views/crud/fields/address_dadata.blade.php
@@ -10,41 +10,41 @@ if (isset($field['value']) && (is_array($field['value']) || is_object($field['va
 ?>
 
 @include('skote::crud.fields.inc.wrapper_start')
-    <label class="control-label">{!! $field['label'] !!}</label>
-    @include('skote::crud.fields.inc.translatable_icon')
-    <input type="hidden"
-           value="{{ old($field['name']) ? old($field['name']) : (isset($field['value']) ? $field['value'] : (isset($field['default']) ? $field['default'] : '' )) }}"
-           name="{{ $field['name'] }}">
+<label class="control-label">{!! $field['label'] !!}</label>
+@include('skote::crud.fields.inc.translatable_icon')
+<input type="hidden"
+       value="{{ old($field['name']) ? old($field['name']) : (isset($field['value']) ? $field['value'] : (isset($field['default']) ? $field['default'] : '' )) }}"
+       name="{{ $field['name'] }}">
 
-    @if(isset($field['prefix']) || isset($field['suffix']))
-        <div class="input-group"> @endif
-            @if(isset($field['prefix']))
-                <div class="input-group-addon">{!! $field['prefix'] !!}</div> @endif
-            @if(isset($field['store_as_json']) && $field['store_as_json'])
-                <input
-                        type="text"
-                        data-dadata-address="{&quot;field&quot;: &quot;{{$field['name']}}&quot;, &quot;full&quot;: {{isset($field['store_as_json']) && $field['store_as_json'] ? 'true' : 'false'}} }"
-                        data-init-function="bpFieldInitAddressDadataElement"
-                        @include('skote::crud.fields.inc.attributes')
-                >
-            @else
-                <input
-                        type="text"
-                        data-dadata-address="{&quot;field&quot;: &quot;{{$field['name']}}&quot;, &quot;full&quot;: {{isset($field['store_as_json']) && $field['store_as_json'] ? 'true' : 'false'}} }"
-                        data-init-function="bpFieldInitAddressDadataElement"
-                        name="{{ $field['name'] }}"
-                        value="{{ old($field['name']) ? old($field['name']) : (isset($field['value']) ? $field['value'] : (isset($field['default']) ? $field['default'] : '' )) }}"
-                        @include('skote::crud.fields.inc.attributes')
-                >
-            @endif
-            @if(isset($field['suffix']))
-                <div class="input-group-addon">{!! $field['suffix'] !!}</div> @endif
-            @if(isset($field['prefix']) || isset($field['suffix'])) </div> @endif
+@if(isset($field['prefix']) || isset($field['suffix']))
+    <div class="input-group"> @endif
+        @if(isset($field['prefix']))
+            <div class="input-group-addon">{!! $field['prefix'] !!}</div> @endif
+        @if(isset($field['store_as_json']) && $field['store_as_json'])
+            <input
+                    type="text"
+                    data-dadata-address="{&quot;field&quot;: &quot;{{$field['name']}}&quot;, &quot;full&quot;: {{isset($field['store_as_json']) && $field['store_as_json'] ? 'true' : 'false'}} }"
+                    data-init-function="bpFieldInitAddressDadataElement"
+                    @include('skote::crud.fields.inc.attributes')
+            >
+        @else
+            <input
+                    type="text"
+                    data-dadata-address="{&quot;field&quot;: &quot;{{$field['name']}}&quot;, &quot;full&quot;: {{isset($field['store_as_json']) && $field['store_as_json'] ? 'true' : 'false'}} }"
+                    data-init-function="bpFieldInitAddressDadataElement"
+                    name="{{ $field['name'] }}"
+                    value="{{ old($field['name']) ? old($field['name']) : (isset($field['value']) ? $field['value'] : (isset($field['default']) ? $field['default'] : '' )) }}"
+                    @include('skote::crud.fields.inc.attributes')
+            >
+        @endif
+        @if(isset($field['suffix']))
+            <div class="input-group-addon">{!! $field['suffix'] !!}</div> @endif
+        @if(isset($field['prefix']) || isset($field['suffix'])) </div> @endif
 
-    {{-- HINT --}}
-    @if (isset($field['hint']))
-        <p class="help-block">{!! $field['hint'] !!}</p>
-    @endif
+{{-- HINT --}}
+@if (isset($field['hint']))
+    <p class="help-block">{!! $field['hint'] !!}</p>
+@endif
 @include('skote::crud.fields.inc.wrapper_end')
 
 {{-- Note: you can use  to only load some CSS/JS once, even though there are multiple instances of it --}}
@@ -91,6 +91,17 @@ if (isset($field['value']) && (is_array($field['value']) || is_object($field['va
                     /* Вызывается, когда пользователь выбирает одну из подсказок */
                     onSelect: function(suggestion) {
                         $field.val(JSON.stringify(suggestion));
+                    },
+                    onSelectNothing:function (){
+                        $field.val("");
+                        element.val("");
+                    }
+                });
+
+                element.change(function(){
+                    console.info(element.val());
+                    if (!element.val().length) {
+                        $field.val("");
                     }
                 });
             }
